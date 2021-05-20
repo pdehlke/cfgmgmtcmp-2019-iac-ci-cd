0edc33ae106390608e2c0f2e8c8f0d3bddd3667d
pdehlke <pde@rfc822.net>
2021-05-20 08:10:43 -0700

    Update backend_config.tf

--- packer-vpc/backend_config.tf
+++ packer-vpc/backend_config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-tfstate-670151543201"
+    bucket         = "terraform-tfstate-abvprp-dev-pde-testing-jenkins"
     key            = "packer-vpc"
     region         = "us-east-1"
     dynamodb_table = "terraform_locks"
